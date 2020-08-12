6bbc481798e1f1f7d307d81615497b9b778e9250
Rubnawaz Qureshi <m.rubnawazqureshi@gmail.com>
2020-08-12 12:08:55 -0700

    Add Validation in Stories Endpoint

--- routes/stories.js
+++ routes/stories.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const con = require('../config/db');
 const jwt = require('jsonwebtoken');
+const validUrl = require('valid-url');
 const verifyToken = require('../middlewares/auth');
 
 router.get('/', (req, res) => {
@@ -15,13 +16,27 @@ router.get('/', (req, res) => {
     });
 });
 
+
 router.post('/', verifyToken, (req, res) => {
     jwt.verify(req.token, process.env.privateKey, (err, authData) => {
         if(err) throw err;
         const { title, redirectLink, community_id } = req.body;
+
+        if(title.length === 0) {
+            return res.json({ success: false, message: 'Title is require' })
+        }
+
+        if(community_id === 0) {
+            return res.json({ success: false, message: 'Please select any one from the list of communities' })
+        }        
+
+        if(validUrl.isUri(redirectLink) !== true) {
+            return res.json({ success: false, message: 'URL is invalid.' })
+        }
+
         const sql = 'INSERT INTO stories (storytitle, redirectLink, community_id, user_id) VALUES (?, ?, ?, ?)';
         con.query(sql, [title, redirectLink, community_id, authData.user.userid], (err, result) => {
-            if(err) return res.send({ success: false, error: err.sqlMessage });
+            if(err) return res.send({ success: false, message: err.sqlMessage });
             res.send({
                 success: true,
                 data: result
