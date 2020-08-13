b3898af62170048eaccea7f1c663881d6a36038f
Rubnawaz Qureshi <m.rubnawazqureshi@gmail.com>
2020-08-13 11:49:35 -0700

    Add S3 file Upload Functionality

--- routes/upload.js
+++ routes/upload.js
@@ -4,79 +4,44 @@ const conn =  require('../config/db');
 const jwt = require('jsonwebtoken');
 const verifyToken = require('../middlewares/auth');
 const fileuploader = require('../utils/fileuploader');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+AWS.config.update({region: 'us-east-1'});
 
-router.post('/', (req, res) => {
-    // initialize fileuploader
-    var uploader = fileuploader('files', {
-        // options will go here
 
-        // limit of files {null, Number}
-        // also with the preloaded files
-        // important to set 1 if file input has no multiple attribute
-        // if null - has no limits
-        // example: 3
-        limit: null,
+router.post('/', multipartMiddleware, verifyToken, (req, res) => {
 
-        // file's maximal size in MB {null, Number}
-        // also with the preloaded files
-        // if null - has no limits
-        // example: 2
-        maxSize: null,
+    const fileName = uuidv4() + path.extname(req.files.file.originalFilename);
 
-        // each file's maximal size in MB {null, Number}
-        // if null - has no limits
-        // example: 2
-        fileMaxSize: null,
+    var fileStream = fs.createReadStream(req.files.file.path);
+    fileStream.on('error', function(err) {
+        console.log('File Error', err);
+    });
 
-        // allowed extensions or file types {null, Array}
-        // if null - has no limits
-        // example: ['jpg', 'jpeg', 'png', 'text/plain', 'audio/*']
-        extensions: null,
+    s3 = new AWS.S3();
+
+    const params = {
+        Bucket: 'fnm-uploads', // AWS Bucket Name
+        Key: fileName, // Title of file after upload on aws s3 bucket
+        Body: fileStream // file path where keep in local computer
+    }
+
+    // call S3 to retrieve upload file to specified bucket
+    s3.upload (params, function (err, data) {
+        if (err) res.json({ success: false, message: err })
         
-        // disallowed extensions or file types {null, Array}
-        // if null - has no limits
-        // example: ['jpg', 'jpeg', 'png', 'text/plain', 'audio/*']
-        disallowedExtensions: null,
-
-        // check if files were choosed (minimum 1 file should be choosed)
-        required: false,
-
-        // upload directory {String}
-        // note that main directory is the directory where you are initializing the fileuploader module
-        // example: '../uploads/'
-        uploadDir: 'uploads/',
-
-        // file title {String, Array}
-        // example: 'name' - original file name
-        // example: 'auto' - random text from 12 letters
-        // example: 'my_custom_filename' - custom file name
-        // example: 'my_custom_filename_{random}' - my_custom_filename_(+ random text from 12 letters)
-        // '{random} {file_name} {file_size} {timestamp} {date} {extension}' - variables that can be used to generate a new file name
-        // example: ['auto', 24] - [0] is a string as in the examples above, [1] is the length of the random string
-        title: ['auto', 12],
-    }, req, res)
-
-    // call to process the form (req.body) and to upload the files
-    uploader.upload(function(data) {
-
-        if (data.isSuccess) {
-            var files = data.files;
+        if (data) {
             res.json({
                 success: true,
-                msg: "fileupload successfully",
-                files: files                
+                fileUrl: data.Location
             });
-        } else {
-            var warnings = data.warnings;
-            res.json({
-                success: false,
-                msg: "fileupload failed",
-                warnings: warnings                
-            })
         }
-            
-        res.end()
-    })
+    });
+
 });
 
 module.exports = router;
